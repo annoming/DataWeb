9244ae1b49ecf8355f9bd05b6b9d1f077c57d8fc
mingrui <annoming_2899@163.com>
2022-11-18 09:33:23 +0800

    init project

---
+++ src/main/java/com/atming/dataweb/DataWebApplication.java
@@ -0,0 +1,13 @@
+package com.atming.dataweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atming/dataweb/DataWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atming.dataweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
